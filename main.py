1d50569379518a7ca87b537af06a89a311749919
MihanDinamo <91221768+MihanDin@users.noreply.github.com>
2023-12-08 02:19:12 +0300

    Update main.py
    New methods of drawing

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 from  Settings import *
 import Player
-import math
-from Map import world_map
 from ray_casting import ray_casting
+from drawing import Drawing
 
 pygame.init()
 sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock =pygame.time.Clock()
 player = Player.Player()
+drawing=Drawing(sc)
 
 while(True):
     for event in pygame.event.get():
@@ -17,12 +17,20 @@ while(True):
 
     sc.fill(BLACK)
 
+    if player.jump:
+        player.jump_func()
+    if player.shift:
+        player.shift_func()
+
     player.move()
-    ray_casting(sc,(player.x,player.y),player.angle)
+    drawing.background()
+
+    drawing.world(player.pos(),player.angle, player.proj_coeff)
+    drawing.fps(clock)
     # pygame.draw.circle(sc,GREEN,(int(player.x),int(player.y)),12)
     # pygame.draw.line(sc,GREEN,(int(player.x),int(player.y)),((player.x+WIDTH*math.cos(player.angle)),(player.y+WIDTH*math.sin(player.angle))))
 
     # for x,y in world_map:
     #     pygame.draw.rect(sc,GREEN,(x,y,TILE,TILE), 2)
     pygame.display.flip()
-    clock.tick(FPS)
+    clock.tick()
