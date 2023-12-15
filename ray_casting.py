3361b6c61643f2cd749d161a1ac17e6b5d731ced
Alexander Kuzmichev <alexk4zmich@gmail.com>
2023-12-15 06:17:23 +0300

    Переработана отрисовка мира, добавлены текстуры, более оптимизированно

--- ray_casting.py
+++ ray_casting.py
@@ -1,14 +1,16 @@
-import math
-
 import pygame
-from Settings import *
-from Map import world_map
+from settings import *
+from map import WORLD_WIDTH, WORLD_HEIGHT
+
 
 def mapping(a, b):
     return (a // TILE) * TILE, (b // TILE) * TILE
 
-def ray_casting(sc, player_pos, player_angle,player_proj_coeff):
+
+def ray_casting(player_pos, player_angle, world_map, player_proj_coeff, player):
+    casted_walls = []
     ox, oy = player_pos
+    texture_v, texture_h = 1, 1
     xm, ym = mapping(ox, oy)
     cur_angle = player_angle - HALF_FOV
     for ray in range(NUM_RAYS):
@@ -18,41 +20,61 @@ def ray_casting(sc, player_pos, player_angle,player_proj_coeff):
         cos_a = cos_a if cos_a else 0.000001
 
         # verticals
-        if cos_a > 0:
-            x = xm+TILE
-            dx=1
-        else:
-            x=xm
-            dx=-1
-
-        for i in range(0, WIDTH, TILE):
+        tile_h = (0, 0)
+        tile_v = (0, 0)
+        x, dx = (xm + TILE, 1) if cos_a >= 0 else (xm, -1)
+        for i in range(0, WORLD_WIDTH, TILE):
             depth_v = (x - ox) / cos_a
-            y = oy + depth_v * sin_a
-            if mapping(x + dx, y) in world_map:
+            yv = oy + depth_v * sin_a
+            tile_v = mapping(x + dx, yv)
+            if tile_v in world_map:
+                texture_v = world_map[tile_v]
                 break
             x += dx * TILE
 
         # horizontals
-        if (sin_a > 0):
-            y = ym + TILE
-            dy = 1
-        else:
-            y = ym
-            dy = -1
-        for i in range(0, HEIGHT, TILE):
+        y, dy = (ym + TILE, 1) if sin_a >= 0 else (ym, -1)
+        for i in range(0, WORLD_HEIGHT, TILE):
             depth_h = (y - oy) / sin_a
-            x = ox + depth_h * cos_a
-            if mapping(x, y + dy) in world_map:
+            xh = ox + depth_h * cos_a
+            tile_h = mapping(xh, y + dy)
+            if tile_h in world_map:
+                texture_h = world_map[tile_h]
                 break
             y += dy * TILE
 
         # projection
-        depth = depth_v if depth_v < depth_h else depth_h
-        depth *= math.cos(player_angle - cur_angle) 
-        depth += 0.01
-        proj_coeff = player_proj_coeff / depth
-        proj_coeff1 = PROJ_COEFF / depth
-        c = 255 / (1 + depth**2*0.0001)
-        color = (c, c, c)
-        pygame.draw.rect(sc, color, (ray*SCALE, HALF_HEIGHT-proj_coeff/2, SCALE, proj_coeff1))
-        cur_angle += DELTA_ANGELS
+        depth, offset, texture = (depth_v, yv, texture_v) if depth_v < depth_h else (depth_h, xh, texture_h)
+        offset = int(offset) % TILE
+        depth *= math.cos(player_angle - cur_angle)
+        depth = max(depth, 0.00001)
+        proj_height = min(int(player_proj_coeff / depth), PENTA_HEIGHT)
+        proj_height_texture = min(int(PROJ_COEFF / depth), PENTA_HEIGHT)
+
+        casted_walls.append((depth, offset, proj_height, proj_height_texture, texture))
+
+        if texture == 5 and depth <= 60:
+            player.take_eye_bool = True
+            if texture_h == 5:
+                pos = tile_h
+            if texture_v == 5:
+                pos = tile_v
+            player.pos_of_eye = pos
+        else:
+            player.take_eye_bool = False
+
+        cur_angle += DELTA_ANGLE
+
+    return casted_walls
+
+
+def ray_casting_walls(player, textures, map):
+    casted_walls = ray_casting(player.pos, player.angle, map.map, player.proj_coeff, player)
+    walls = []
+    for ray, casted_values in enumerate(casted_walls):
+        depth, offset, proj_height, proj_height_texture, texture = casted_values
+        wall_column = textures[texture].subsurface(offset * TEXTURE_SCALE, 0, TEXTURE_SCALE, TEXTURE_HEIGHT)
+        wall_column = pygame.transform.scale(wall_column, (SCALE, proj_height_texture))
+        wall_pos = (ray * SCALE, HALF_HEIGHT - proj_height // 2)
+        walls.append((depth, wall_column, wall_pos))
+    return walls
