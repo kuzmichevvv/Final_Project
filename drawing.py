afe3263690aa03a1bc598bbe3ad0fe8dec1e0d1a
Alexander Kuzmichev <alexk4zmich@gmail.com>
2023-12-15 06:16:14 +0300

    Улучшена отрисовка, добавлена отрисовка монстра, мимни карты, и текстур

--- drawing.py
+++ drawing.py
@@ -1,22 +1,47 @@
 import pygame
-
-from Settings import *
+from settings import *
 from ray_casting import ray_casting
+from map import mini_map
 
 
 class Drawing:
-    def __init__(self,sc):
+    def __init__(self, sc, sc_map):
         self.sc = sc
-        self.font = pygame.font.SysFont('Arial',36, bold=True)
+        self.sc_map = sc_map
+        self.font = pygame.font.SysFont('Arial', 36, bold=True)
+        self.textures = {1: pygame.image.load('img/wall3.png').convert(),
+                         2: pygame.image.load('img/wall4.png').convert(),
+                         3: pygame.image.load('img/wall5.png').convert(),
+                         4: pygame.image.load('img/wall6.png').convert(),
+                         5: pygame.image.load('img/eye_1.png').convert(),
+                         6: pygame.image.load('img/eye.png').convert(),
+                        'S': pygame.image.load('img/sky2_1.png').convert()
+                         }
 
-    def background(self):
-        pygame.draw.rect(self.sc, LIGHTBLACK, (0, 0, WIDTH, HALF_HEIGHT))
+    def background(self, angle):
+        sky_offset = -10 * math.degrees(angle) % WIDTH
+        self.sc.blit(self.textures['S'], (sky_offset, 0))
+        self.sc.blit(self.textures['S'], (sky_offset - WIDTH, 0))
+        self.sc.blit(self.textures['S'], (sky_offset + WIDTH, 0))
         pygame.draw.rect(self.sc, DARKGRAY, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
 
-    def world(self, player_pos, player_angle, player_proj_coeff):
-        ray_casting(self.sc, player_pos, player_angle, player_proj_coeff)
+    def world(self, world_objects):
+        for obj in sorted(world_objects, key=lambda n: n[0], reverse=True):
+            if obj[0]:
+                _, object, object_pos = obj
+                self.sc.blit(object, object_pos)
 
-    def fps(self,clock):
+    def fps(self, clock):
         display_fps = str(int(clock.get_fps()))
-        render = self.font.render(display_fps, 0, RED)
+        render = self.font.render(display_fps, 0, DARKORANGE)
         self.sc.blit(render, FPS_POS)
+
+    def mini_map(self, player):
+        self.sc_map.fill(BLACK)
+        map_x, map_y = player.x // MAP_SCALE, player.y // MAP_SCALE
+        pygame.draw.line(self.sc_map, YELLOW, (map_x, map_y), (map_x + 12 * math.cos(player.angle),
+                                                 map_y + 12 * math.sin(player.angle)), 2)
+        pygame.draw.circle(self.sc_map, RED, (int(map_x), int(map_y)), 5)
+        for x, y in mini_map:
+            pygame.draw.rect(self.sc_map, DARKBROWN, (x, y, MAP_TILE, MAP_TILE))
+        self.sc.blit(self.sc_map, MAP_POS)
