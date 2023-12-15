bdb4e6f9e727ae79ceb12f752a4c6aaf1a5a30e5
Alexander Kuzmichev <alexk4zmich@gmail.com>
2023-12-15 06:15:05 +0300

    Карта улучшена, доработана и увеличена

--- map.py
+++ map.py
@@ -1,22 +1,52 @@
-from Settings import *
+from settings import *
 import pygame
 
 
-text_map =[
-'WWWWWWWWWWWW',
-'W..........W',
-'W.....W....W',
-'W..........W',
-'W...WW.....W',
-'W..........W',
-'W..........W',
-'WWWWWWWWWWWW'
-]
-world_map = set()
-collision_walls = []
+class Map:
+    def __init__(self, world_map):
+        self.map = world_map
 
-for j, row in enumerate(text_map):
+
+_ = False
+matrix_map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, _, _, _, _, _, 2, _, _, _, _, _, _, _, _, _, _, 4, _, _, _, _, _, 1],
+    [1, _, 5, 2, _, _, _, _, _, 2, 2, 2, _, _, _, 5, _, _, _, _, 4, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, _, 2, 2, _, _, _, 3, _, _, _, _, _, _, 1],
+    [1, _, 2, 2, _, _, _, _, _, _, _, _, 2, _, 4, _, _, 3, _, _, _, 4, _, 1],
+    [1, _, _, _, _, _, 4, _, _, 2, 2, _, 2, _, _, _, _, _, _, 4, _, _, _, 1],
+    [1, _, 3, _, _, _, 2, _, _, 2, _, _, 2, _, _, _, 4, _, _, _, _, 4, _, 1],
+    [1, _, _, 3, _, _, 2, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, 3, _, _, _, _, _, _, _, 3, _, _, 3, 3, _, _, _, _, 3, 5, _, _, 1],
+    [1, _, 3, _, _, _, 5, 3, _, 3, _, _, _, 3, 3, _, _, _, _, 2, 3, _, _, 1],
+    [1, _, _, _, _, 3, _, 3, _, _, 3, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, _, 4, _, 3, _, _, _, _, 3, _, _, 2, _, _, _, _, _, _, _, _, 2, _, 1],
+    [1, _, _, _, _, _, 4, _, _, _, _, _, 5, 2, _, _, _, _, _, _, 2, 2, _, 1],
+    [1, _, _, 4, _, _, _, _, 4, _, _, _, _, 2, 2, 2, 2, 2, 2, 2, 2, _, _, 1],
+    [1, _, _, _, _, _, _, _, _, _, 4, _, _, _, _, _, _, _, _, _, _, _, _, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+WORLD_WIDTH = len(matrix_map[0]) * TILE
+WORLD_HEIGHT = len(matrix_map) * TILE
+world_map = {}
+mini_map = set()
+collision_walls = []
+for j, row in enumerate(matrix_map):
     for i, char in enumerate(row):
-        if char == 'W':
-            world_map.add((i*TILE, j*TILE))
+        if char:
+            mini_map.add((i * MAP_TILE, j * MAP_TILE))
             collision_walls.append(pygame.Rect(i * TILE, j * TILE, TILE, TILE))
+            if char == 1:
+                world_map[(i * TILE, j * TILE)] = 1
+            elif char == 2:
+                world_map[(i * TILE, j * TILE)] = 2
+            elif char == 3:
+                world_map[(i * TILE, j * TILE)] = 3
+            elif char == 4:
+                world_map[(i * TILE, j * TILE)] = 4
+            elif char == 5:
+                world_map[(i * TILE, j * TILE)] = 5
+for i in range(len(matrix_map[0])):
+    mini_map.add((i * MAP_TILE, 15 * MAP_TILE))
+print(world_map)
